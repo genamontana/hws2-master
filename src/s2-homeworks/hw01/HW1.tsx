0f74cac5aeffeb6f81fafae57d5567c475eebc1c
genamontana <predator199@yandex.ru>
2022-10-21 13:23:09 +0300

    issued friend message module, added to the component message

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -64,6 +64,7 @@ const HW1 = () => {
                 <div>
                     <Message message={message0} />
                     <FriendMessage message={friendMessage0} />
+                    <Message message={message0} />
                 </div>
 
                 {/*для автоматической проверки дз (не менять)*/}
