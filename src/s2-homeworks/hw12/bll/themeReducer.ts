fdab192c34a239b48ef71efc9100e0425775e21c
genamontana <predator199@yandex.ru>
2023-04-21 17:12:25 +0300

    HW 12 completed

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,24 @@
+type initStateType = {
+    themeId: number
+}
+
 const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+
+export const themeReducer = (state = initState, action: ChangeThemeIdActionType): initStateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID':
+                return {...state,
+                    themeId: action.id
+                }
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+export type ChangeThemeIdActionType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id: number): {type: string, id: number} => ({ type: 'SET_THEME_ID', id } as const) // fix any
